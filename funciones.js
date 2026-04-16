006fe96fdd05b4d32fff7540546ecf13e5e95bab
xavopalacios <xavopalacios@gmail.com>
2026-04-15 21:27:57 -0500

    feat: the funtion were added

--- funciones.js
+++ funciones.js
@@ -1,4 +1,31 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
-calcularDisponible = (ingresos, egresos) => {
-    return ingresos - egresos;
+function calcularDisponible (ingresos, egresos) {
+   if (ingresos && egresos && ingresos>egresos) {
+       return ingresos - egresos;
+   }
+   return 0;
+} 
+
+function calcularCapacidadPago (montoDisponible) {
+ return montoDisponible * 0.5;
+
 }
+
+function calcularInteresSimple(Monto,Tasa,plazoAnios){
+    return Monto*plazoAnios*Tasa/100;
+}
+
+function calcularTotalPagar(monto,interes){
+    return monto + interes +100;
+}
+
+function calcularCuotaMensual(total,plazoAnios){
+    return total / (plazoAnios * 12);
+}
+
+function aprobarCredito(capacidadPago, cuotaMensual) {
+    if (capacidadPago >= cuotaMensual) {
+        return "CERDITO APROBADO";
+    } 
+        return "CERDITO RECHAZADO";
+    }
--- simulador.js
+++ simulador.js
@@ -1,2 +1,30 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
-calcular
+ 
+function calcular() {
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);     
+    
+    let montoDisponible = calcularDisponible(ingresos, egresos);
+    document.getElementById("spnDisponible").innerText =  montoDisponible
+
+    let capacidadPago = calcularCapacidadPago(montoDisponible);
+    document.getElementById("spnCapacidadPago").innerText = capacidadPago;
+
+    let montoCredito = parseFloat(document.getElementById("txtMonto").value);
+    let tasaInteres = parseFloat(document.getElementById("txtTasaInteres").value);
+    let plazoAnios = parseFloat(document.getElementById("txtPlazo").value);
+    let interes = calcularInteresSimple(montoCredito, tasaInteres, plazoAnios);
+    document.getElementById("spnInteresPagar").innerText = interes;
+
+    let totalPagar = calcularTotalPagar(montoCredito, interes);
+    document.getElementById("spnTotalPrestamo").innerText = totalPagar;
+
+    let pagoMensual = calcularCuotaMensual(totalPagar, plazoAnios).toFixed(2);
+    document.getElementById("spnCuotaMensual").innerText = pagoMensual;    
+    
+    let estadoCredito = aprobarCredito(capacidadPago, pagoMensual);
+    document.getElementById("spnEstadoCredito").innerText = estadoCredito;
+
+
+}
+
